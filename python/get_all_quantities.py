6bb7e8c9dae7b9726f45e89eefaec932f553c1a6
David Collins <dccollins@fsu.edu>
2022-09-19 10:30:45 -0400

    add more stuff

--- python/get_all_quantities.py
+++ python/get_all_quantities.py
@@ -1,7 +1,11 @@
 from GL import *
 from collections import defaultdict
 
-def return_average_quantities(file_list=[], all_quan = defaultdict(lambda: list())):
+def return_average_quantities(file_list=[], all_quan = None):
+    if all_quan is None:
+        all_quan = defaultdict(lambda: list())
+    else:
+        all_quan = list(all_quan)
     for this_name in file_list:
         if not os.path.exists(this_name):
             print("No such file, skipping: %s"%this_name)
@@ -38,8 +42,6 @@ def files_from_output(path_to_output_log, suffix=".AverageQuantities.h5"):
 
 def all_quan_from_outputlog(output_log):
     file_list= files_from_output(output_log)
-    
-
     quan = return_average_quantities(file_list)
     return quan
 
@@ -50,3 +52,40 @@ def all_quan_from_taxi(car):
         file_list.append("%s/%s.AverageQuantities.h5"%(car.directory,car.frame_dict[n]['dsname']))
     all_quan = return_average_quantities(file_list)
     return all_quan
+
+def get_quantities_and_rms(output_log):
+    quan = all_quan_from_outputlog(output_log)
+    
+    mytime=quan['time']
+    mytime -= mytime[0]
+    mytime /= mytime[-1]
+    quan['mytime']=mytime
+
+    vx2 = quan['vx_std']**2
+    vy2 = quan['vy_std']**2
+    vz2 = quan['vz_std']**2
+    quan['vrms'] = np.sqrt(vx2 + vy2 + vz2)
+    quan['Ms'] = quan['vrms']
+
+    vax2 = quan['alf_x_std']**2
+    vay2 = quan['alf_y_std']**2
+    vaz2 = quan['alf_z_std']**2
+    quan['varms']= np.sqrt(vax2 + vay2 + vaz2)
+
+    Bx = quan['bx_avg']
+    By = quan['by_avg']
+    Bz = quan['bz_avg']
+    Btot = np.sqrt( Bx**2+By**2+Bz**2)
+    quan['Btot'] = Btot
+
+    quan['Ma'] = quan['vrms']/quan['Btot']**2
+
+    bx = quan['bx_std']**2
+    by = quan['by_std']**2
+    bz = quan['bz_std']**2
+    brms = np.sqrt( bx + by + bz)
+    #ax_b2v2.scatter( vrms.mean(), 1/(vrms.mean()/brms.mean()/Btot.mean()), c=sim_colors.color[sim],marker=sim_colors.marker[sim]) 
+    quan['brms'] = brms
+    quan['also_brms'] = quan['vrms']**2/quan['Btot']
+    return quan
+
