d43d7d003a76f99640c29c893f21ada33e0ea49f
David Collins <dccollins@fsu.edu>
2022-09-19 10:29:31 -0400

    tweaks to amps slopes

--- python/amps_slopes.py
+++ python/amps_slopes.py
@@ -43,7 +43,8 @@ if 0:
 #
 LOS = 'y'
 range_ext=dt.extents()
-simlist = ['half_half','1_half','2_half','3_half']#['1_half']#['half_2', 'half_half']#['half_half']#['1_1']#['2_2'] #['half_half'] # ['half_half']#,'2_2']
+simlist = ['half_half']#,'1_half','2_half','3_half']#['1_half']#['half_2', 'half_half']#['half_half']#['1_1']#['2_2'] #['half_half'] # ['half_half']#,'2_2']
+simlist = sim_colors.simlist
 if 1:
     plt.close('all')
     #fig,ax = plt.subplots(1,3, sharex=True,sharey=True,figsize=(12,4))
@@ -51,78 +52,78 @@ if 1:
     fig.subplots_adjust(wspace=0, hspace=0)
     axlist=ax.flatten()
 
-    for nf,field in enumerate(['avg_cltt','avg_clee','avg_clbb']):
-        print('aaa')
-        for sim in simlist: #sim_colors.simlist:
+    for sim in simlist: #sim_colors.simlist:
 
-            do_prim=False; do_TEB=False
-            do_amp=False; do_slope=False
+        do_prim=False; do_TEB=False
+        do_amp=False; do_slope=False
 
-            if 1:
-                F1 = 'avg_cltt'
-                F2 = 'avg_clee'
-                F3 = 'avg_clbb'
-                S1 = 'TT'
-                S2 = 'EE'
-                S3 = 'BB'
-                which_quan = "TEB_%s"%LOS
-                do_TEB=True
-            else:
-                F1 = 'avg_d'
-                F2 = 'avg_v'
-                F3 = 'avg_h'
-                S1 = r'\rho'
-                S2 = 'v'
-                S3 = 'h'
-                which_quan = "prim"
-                do_prim=True
+        if 1:
+            F1 = 'avg_cltt'
+            F2 = 'avg_clee'
+            F3 = 'avg_clbb'
+            S1 = 'TT'
+            S2 = 'EE'
+            S3 = 'BB'
+            which_quan = "TEB_%s"%LOS
+            do_TEB=True
+        else:
+            F1 = 'avg_d'
+            F2 = 'avg_v'
+            F3 = 'avg_h'
+            S1 = r'\rho'
+            S2 = 'v'
+            S3 = 'h'
+            which_quan = "prim"
+            do_prim=True
 
-            #
-            # slopes or amplitudes
-            #
-            do_log=False
-            if 0:
-                #amplitudes
-                do_log=True
-                do_amp=True
-                the_y_T = spectra_dict[LOS][sim].amps[F1]
-                the_y_E = spectra_dict[LOS][sim].amps[F2]
-                the_y_B = spectra_dict[LOS][sim].amps[F3]
-                aos = 'amps'
-                Aalpha = 'A'
-            elif 1:
-                #slopes
-                do_slope=True
-                the_y_T = spectra_dict[LOS][sim].slopes3[field].AlphaPeak
-                the_y_E = spectra_dict[LOS][sim].slopes3[field].AlphaPeak
-                the_y_B = spectra_dict[LOS][sim].slopes3[field].AlphaPeak
-                #the_y_T = spectra_dict[LOS][sim].slope_bucket.avg_slope[F1]
-                #the_y_E = spectra_dict[LOS][sim].slope_bucket.avg_slope[F2]
-                #the_y_B = spectra_dict[LOS][sim].slope_bucket.avg_slope[F3]
-                #the_std_T = spectra_dict[LOS][sim].slope_bucket.std_slope[F1]
-                #the_std_E = spectra_dict[LOS][sim].slope_bucket.std_slope[F2]
-                #the_std_B = spectra_dict[LOS][sim].slope_bucket.std_slope[F3]
-                #the_y_T = spectra_dict[LOS][sim].slopes[F1]
-                #the_y_E = spectra_dict[LOS][sim].slopes[F2]
-                #the_y_B = spectra_dict[LOS][sim].slopes[F3]
-                aos = 'slopes'
-                Aalpha = "\\alpha"
+        #
+        # slopes or amplitudes
+        #
+        do_log=False
+        if 0:
+            #amplitudes
+            do_log=True
+            do_amp=True
+            the_y_T = spectra_dict[LOS][sim].amps[F1]
+            the_y_E = spectra_dict[LOS][sim].amps[F2]
+            the_y_B = spectra_dict[LOS][sim].amps[F3]
+            aos = 'amps'
+            Aalpha = 'A'
+        elif 1:
+            #slopes
+            do_slope=True
+            suffix='AlphaFixed_25'
+            suffix='AlphaPeak'
+            the_y_T = spectra_dict[LOS][sim].slopes3[F1].AlphaPeak
+            the_y_E = spectra_dict[LOS][sim].slopes3[F2].AlphaPeak
+            the_y_B = spectra_dict[LOS][sim].slopes3[F3].AlphaPeak
+            #the_y_T = spectra_dict[LOS][sim].slope_bucket.avg_slope[F1]
+            #the_y_E = spectra_dict[LOS][sim].slope_bucket.avg_slope[F2]
+            #the_y_B = spectra_dict[LOS][sim].slope_bucket.avg_slope[F3]
+            #the_std_T = spectra_dict[LOS][sim].slope_bucket.std_slope[F1]
+            #the_std_E = spectra_dict[LOS][sim].slope_bucket.std_slope[F2]
+            #the_std_B = spectra_dict[LOS][sim].slope_bucket.std_slope[F3]
+            #the_y_T = spectra_dict[LOS][sim].slopes[F1]
+            #the_y_E = spectra_dict[LOS][sim].slopes[F2]
+            #the_y_B = spectra_dict[LOS][sim].slopes[F3]
+            aos = 'slopes'
+            Aalpha = "\\alpha"
 
-            this_Ms = quan3[sim]['msavg'].mean()
-            this_Ma = quan3[sim]['maavg'].mean()
-            kwargs = {"c":sim_colors.color[sim], "marker":sim_colors.marker[sim]}
-            print(sim, sim_colors.color[sim], the_y_T)
-            ax[0][0].scatter(this_Ms, the_y_T,  **kwargs)
-            print('b')
-            ax[1][0].scatter(this_Ms, the_y_E,  **kwargs)
-            ax[2][0].scatter(this_Ms, the_y_B,  **kwargs)
+        this_Ms = quan3[sim]['msavg'].mean()
+        this_Ma = quan3[sim]['maavg'].mean()
+        kwargs = {"c":sim_colors.color[sim], "marker":sim_colors.marker[sim]}
+        print(sim, sim_colors.color[sim], the_y_T)
+        ax[0][0].scatter(this_Ms, the_y_T,  **kwargs)
+        print('b', this_Ms)
+        ax[1][0].scatter(this_Ms, the_y_E,  **kwargs)
+        ax[2][0].scatter(this_Ms, the_y_B,  **kwargs)
 
-            ax[0][1].scatter(this_Ma, the_y_T,  **kwargs)
-            ax[1][1].scatter(this_Ma, the_y_E,  **kwargs)
-            ax[2][1].scatter(this_Ma, the_y_B,  **kwargs)
-            range_ext(the_y_T)
-            range_ext(the_y_E)
-            range_ext(the_y_B)
+        ax[0][1].scatter(this_Ma, the_y_T,  **kwargs)
+        ax[1][1].scatter(this_Ma, the_y_E,  **kwargs)
+        ax[2][1].scatter(this_Ma, the_y_B,  **kwargs)
+        range_ext(the_y_T)
+        range_ext(the_y_E)
+        range_ext(the_y_B)
 
     if do_log:
         for aaa in axlist:
@@ -166,6 +167,6 @@ if 1:
 
             
 
-    outname = '%s/%s_mach_%s_%s.pdf'%(plotdir, which_quan,aos,'measured3')
+    outname = '%s/%s_mach_%s_%s.pdf'%(plotdir, which_quan,aos,suffix)
     fig.savefig(outname)
     print(outname)
