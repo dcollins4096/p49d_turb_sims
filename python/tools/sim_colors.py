6351ef3dd47eb80a41b1f97f8e52cf122b28503b
David Collins <dccollins@fsu.edu>
2022-09-27 12:59:50 -0400

    sim_colors space for kludges

--- python/tools/sim_colors.py
+++ python/tools/sim_colors.py
@@ -8,6 +8,7 @@ cloudbreak_128 = "/data/cb1/Projects/P49_EE_BB/Downsample128"
 
 #these need to stay in this order.
 simlist=nar(["half_half","half_1","half_2","1_half","1_1","1_2","2_half","2_1","2_2","3_half","3_1","3_2","5_half","5_1","5_2"])
+#simlist=nar(["5_half"]); print('kludge in simlist')
 #simlist=nar(["1_1"])
 color_list=nar(['r','g','b','r','g','b','r','g','b','r','g','b','r','g','b'])
 line_list=nar(['-','-','-','-.','-.','-.','--','--','--',':',':',':', '-','-','-'])
@@ -17,6 +18,7 @@ marker_list = nar(['.','.','.','s','s','s','^','^','^','*','*','*','o','o','o'])
 
 framedict={
     "half_half":range(11,31),"half_1":range(11,31),"half_2":range(11,31),"1_half":range(11,31),"1_1":range(11,31),"1_2":range(11,31),"2_half":range(65,85),"2_1":range(11,31),"2_2":range(11,31),"3_half":range(72,91),"3_1":range(56,75),"3_2":range(20,40),"5_half":range(3,36),"5_1":range(4,27),"5_2":range(4,45),"5_3":range(5,58)}
+#framedict['5_half']=range(11,13); print('kludge in framedict')
 
 sim_ms = nar(['half','1','2','3','5'])
 sim_ma = nar(['half','1','2'])
