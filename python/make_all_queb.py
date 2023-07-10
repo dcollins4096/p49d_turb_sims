479bb3c6d717e954be8b617f5a1de66d2d53aeac
David Collins <dccollins@fsu.edu>
2023-07-10 11:15:40 -0400

    improve make_all

--- python/make_all_queb.py
+++ python/make_all_queb.py
@@ -9,30 +9,33 @@ Make many queb data products.
 from GL import *
 import queb3
 reload(queb3)
-frames=list(range(0,500,10)) + list(range(0,10))
-frames=[499]
-sim_dir = "/scratch1/dcollins/Paper49_EBQU/ca02_turb"
-plot_dir =  "/home/dcollins4096/PigPen"
+import sim_colors
+plot_dir =  "/home/dccollins/PigPen"
 
 #a thing that describes the simulation
-pack = queb3.simulation_package( directory=sim_dir,frames=frames,prefix="ca02", BoxSize=2)
+for sim in sim_colors.simlist:
+    frames=sim_colors.framedict[sim]
+    sim_dir = sim_colors.cloudbreak_base + "/" + sim
+    product_dir = sim_colors.cloudbreak_base + "/Products/" + sim
+    pack = queb3.simulation_package( directory=sim_dir,frames=frames,prefix=sim, product_directory=product_dir)
 #produce all QUEB products.
-pack.EBall()
-for frame in frames:
-    #makes 3d spectra
-    pack.make_spectra(frame=frame)
-    #read Q&U.  Compute E&B.
-    this_proj=pack.read_queb(frame=frame,ax='x')
-    this_proj.compute_harmonic_products()
-    #read 3d spectra    
-    this_proj.read_spectra(frame)
-    #here we will make improvements.
-    #Also here we can vary the fit range.
-    #Further, we can average this_proj.ClEE over several frames to 
-    #get a more meaningful result.
-    fitrange=this_proj.determine_fit_range()  #something better 
-    #do the fit.  Not much fancy here
-    slopes=this_proj.fit_eb_slopes(fitrange=fitrange)
-    #this plotting package can be generalized.
-    pack.plot_eb(this_proj, fname='%s/ca02_reb_x_n%04d.png'%(plot_dir,frame),slopes=slopes)
-    pack.plot_many_spectra(this_proj)
+    #pack.EBall()
+    for frame in frames:
+        #makes 3d spectra
+        pack.make_spectra(frame=frame)
+        #read Q&U.  Compute E&B.
+        this_proj=pack.read_queb(frame=frame,ax='x')
+        this_proj.compute_harmonic_products()
+        #read 3d spectra    
+        this_proj.read_spectra(frame)
+        #here we will make improvements.
+        #Also here we can vary the fit range.
+        #Further, we can average this_proj.ClEE over several frames to 
+        #get a more meaningful result.
+        fitrange=this_proj.determine_fit_range()  #something better 
+        #do the fit.  Not much fancy here
+        slopes=this_proj.fit_eb_slopes(fitrange=fitrange)
+        #this plotting package can be generalized.
+        this_proj.prefix=sim
+        pack.plot_eb(this_proj, fname='%s/%s_x_n%04d.png'%(plot_dir,sim,frame),slopes=slopes)
+        pack.plot_many_spectra(this_proj)
