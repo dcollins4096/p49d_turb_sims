244185689fcb111b16b9be7987269d028601dc92
David Collins <dccollins@fsu.edu>
2023-07-11 13:27:41 -0400

    add some things to runner

--- python/data_scrub_2/runner.py
+++ python/data_scrub_2/runner.py
@@ -2,16 +2,21 @@ from  GL import *
 
 import simulation_info.suite_liltest
 reload(simulation_info.suite_liltest)
+import simulation_info.suite_1
+reload(simulation_info.suite_1)
 
 import data_scrub_2.compute_all_quan as caq
 #reload(caq)
-this_simname="4s_dave"
+this_simname="1_1"
 if 0:
     caq.comp_all(this_simname)
 import plots.P1_plot_quan as p1
 reload(p1)
-if 1:
+if 0:
     p1.plot_quan([this_simname])
+if 1:
+    for sim in sim_colors.simlist:
+        p1.plot_quan([sim])
 
 import data_scrub_2.make_all_frb as maf
 reload(maf)
