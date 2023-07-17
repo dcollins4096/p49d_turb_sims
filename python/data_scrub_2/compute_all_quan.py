cee089d983dab1d499ddd401a4903c4af716ec4d
David Collins <dccollins@fsu.edu>
2023-07-17 09:49:40 -0400

    Add clobber flag to compute_all_quan

--- python/data_scrub_2/compute_all_quan.py
+++ python/data_scrub_2/compute_all_quan.py
@@ -5,8 +5,9 @@ reload(sim)
 import compute_avg_quantities as comp_avg
 reload(comp_avg)
 
-def comp_all(sim_name):
-    this_sim = sim.corral[sim_name]
-    for frame in this_sim.framelist:
-        comp_avg.make_quan(this_sim.data_location,frame,out_directory=this_sim.product_location,sim=sim_name )
+def comp_all(simlist):
+    for sim_name in simlist:
+        this_sim = sim.corral[sim_name]
+        for frame in this_sim.framelist:
+            comp_avg.make_quan(this_sim.data_location,frame,out_directory=this_sim.product_location,sim=sim_name, clobber=False )
 
