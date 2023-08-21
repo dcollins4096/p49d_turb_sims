635def10354e77a77e517262f5bfac1d8a42f8c9
David Collins <dccollins@fsu.edu>
2023-08-21 12:13:30 -0400

    update cmap

--- python/tools/sim_colors.py
+++ python/tools/sim_colors.py
@@ -2,11 +2,21 @@ from GL import *
 import davetools as dt
 
 
-mach_norm = mpl.colors.Normalize(vmin=0,vmax=7)
-if 'cmap' not in dir():
-    #cmap='jet'
-    cmap = 'gist_rainbow'
-cbar = matplotlib.cm.ScalarMappable(mach_norm,cmap=cmap)
 mach_label=r'$\mathcal{M}_{\rm{S}}$'
 alf_mach_label=r'$\mathcal{M}_{\rm{A}}$'
+
+mach_norm = mpl.colors.Normalize(vmin=0,vmax=7)
+
+cmap=None
+cbar=None
+
+def set_cmap(cmap_name):
+    global cmap, cbar
+    cmap=cmap_name
+    cbar = matplotlib.cm.ScalarMappable(mach_norm,cmap=cmap_name)
+
+if cmap is None:
+    #cmap='jet'
+    set_cmap( 'gist_rainbow')
+
 
