ef3c0ef6072cb2d7699fd6e0b0647f0a9fe2da3c
David Collins <dccollins@fsu.edu>
2023-08-21 12:13:12 -0400

    comments

--- python/p49_plots/maker.py
+++ python/p49_plots/maker.py
@@ -21,43 +21,24 @@ sim_list = all_sims.lists['suite1']
 
 
 
-if 0:
-    F1.proj(field='density_',LOS='y', cmap='plasma')
-    F1.proj(field='E',LOS='y', no_mean=True, cmap='plasma')
-    F1.proj(field='B',LOS='y', cmap='plasma')
-if 1:
-    F2.nom(sim_list)
-if 0:
-    F3.plot_avg_spectra(sim_list, prim_or_teb='teb', axis='y')
-    F3.plot_avg_spectra(sim_list, prim_or_teb='prim')
-if 1:
-    F4.plot_amps_slopes(sim_list, prim_or_teb='prim',amps_or_slopes='slopes')
-if 1:
-    F4.plot_amps_slopes(sim_list, prim_or_teb='prim',amps_or_slopes='amps')
-    F4.plot_amps_slopes(sim_list, prim_or_teb='teb',amps_or_slopes='slopes', axis='y')
-    F4.plot_amps_slopes(sim_list, prim_or_teb='teb',amps_or_slopes='amps', axis='y')
-    F5.plot_amps_amps(sim_list, amps_or_slopes='amps',axis='y')
-    F5.plot_amps_amps(sim_list, amps_or_slopes='slopes',axis='y')
-    F9.fit_all(sim_list)
-    F6.plot_ratios(sim_list, LOS='y')
-    F6.plot_amps(sim_list,LOS='y')
-    F7.plot_spectra(sim_list,LOS='y')
-    F7.plot_meanvar(sim_list,LOS='y')
-if 1:
-    F8.plot_summary(sim_list, LOS='y')
-if 0:
-    F9.fit_all(simlist)
-
-
-if 0:
-    F2.nom(sim_list)
-
-if 0:
-    #fits along with amps.
-    #sim_list=["%s_1"%s for s in ['half','1','2','3','4','5','6']]
-    #sim_list=["%s_half"%s for s in ['half','1','2','3','4','5','6']]
-    sim_list = all_sims.lists['suite1']
-    herd=F9.fit_all(sim_list, fit34=3)
-    F4.plot_amps_slopes(sim_list, prim_or_teb='teb',amps_or_slopes='slopes', axis='y', fit_herd=herd)
+sim_colors.set_cmap('cool')
+proj_cmap = 'plasma'
+#proj_cmap = 'cool'
+F1.proj(field='density_',LOS='y', cmap=proj_cmap)
+F1.proj(field='E',LOS='y', no_mean=True, cmap=proj_cmap)
+F1.proj(field='B',LOS='y', cmap=proj_cmap)
+F2.nom(sim_list)
+F3.plot_avg_spectra(sim_list, prim_or_teb='teb', axis='y')
+F3.plot_avg_spectra(sim_list, prim_or_teb='prim')
+F4.plot_amps_slopes(sim_list, prim_or_teb='prim',amps_or_slopes='slopes')
+F4.plot_amps_slopes(sim_list, prim_or_teb='prim',amps_or_slopes='amps')
+F4.plot_amps_slopes(sim_list, prim_or_teb='teb',amps_or_slopes='slopes', axis='y')
+F4.plot_amps_slopes(sim_list, prim_or_teb='teb',amps_or_slopes='amps', axis='y')
+F6.plot_ratios(sim_list, LOS='y')
+F6.plot_amps(sim_list,LOS='y')
+F7.plot_spectra(sim_list,LOS='y')
+F7.plot_meanvar(sim_list,LOS='y')
+F8.plot_summary(sim_list, LOS='y')
+#F9.fit_all(sim_list)
 
 
