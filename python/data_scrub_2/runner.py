d5bd17da21322e98a4963e1c9990ce169cb2a56b
David Collins <dccollins@fsu.edu>
2023-09-22 17:04:17 -0400

    tweak a runner

--- python/data_scrub_2/runner.py
+++ python/data_scrub_2/runner.py
@@ -3,26 +3,26 @@ from  GL import *
 import simulation
 import simulation_info.all_sims
 
-import data_scrub_2.compute_all_quan as caq
+#import data_scrub_2.compute_all_quan as caq
 import data_scrub_2.make_2d_spectra as m2d
 import data_scrub_2.make_3d_spectra as m3d
 import data_scrub_2.make_all_frb as maf
-import data_scrub_2.image_frb as imf
+#import data_scrub_2.image_frb as imf
 import plots.P3_all_spectra as p3
 import plots.P1_plot_quan as p1
 #this_simname="1_1"
 #this_simname="4s_dev"
 #this_simname="4s_dave"
-this_list=['4_half','4_2']
+this_list=['4_2']
 
-if 1:
+if 0:
     caq.comp_all(this_list)
 if 0:
     m2d.make_all(this_list)
 if 0:
     maf.make_all(this_list)
-if 0:
-    m3d.make_spec(sim_colors.simlist)
+if 1:
+    m3d.make_spec(this_list)
 
 if 0:
     p1.plot_quan([this_simname])
