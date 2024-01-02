8fcbc05a75b937bab6493e0412258edbfd1cc08e
David Collins <dccollins@fsu.edu>
2024-01-02 10:25:41 -0500

    planck values

--- python/tools/sim_colors.py
+++ python/tools/sim_colors.py
@@ -10,6 +10,10 @@ mach_norm = mpl.colors.Normalize(vmin=0,vmax=7)
 cmap=None
 cbar=None
 
+planck_E_slope = -2.42 #LR71
+planck_B_slope = -2.54 #LR71
+planck_ratio = 0.53 #0.48 to 0.53 for patches.  0.5 is LR42, 0.53 is LR71
+
 def set_cmap(cmap_name):
     global cmap, cbar
     cmap=cmap_name
