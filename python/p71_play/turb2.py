af631d82c6ea8935acc22ce55b979678c873c8f9
David Collins <dccollins@fsu.edu>
2024-01-02 10:24:52 -0500

    p71 play

--- python/p71_play/turb2.py
+++ python/p71_play/turb2.py
@@ -18,13 +18,15 @@ reload(htools)
 plotdir = "%s/PigPen"%(os.environ['HOME'])
 
 
-sim = '4_1'
-frame = 10
+sim = '6_half'
+frame = 36
 
 
 
 if 'esystem' not in dir():
+    print('get')
     rho_full, rho256 = htools.get_cubes(sim,frame)
+    print('esystem')
 
     esystem = htools.eigen_stuff(np.log(rho256))
     esystem.do()
@@ -49,22 +51,24 @@ if 0:
     #removed
     htools.color_game(esystem,"%s/turb_color_game"%plotdir)
 
-if 1:
+if 0:
     #3 eigenvalues in a channel map
     cgames.color_game_1(esystem,"%s/slice_game_e0_e1_e2_%s_n%04d"%(plotdir,sim,frame))
-if 1:
+if 0:
     #individual eigen vectors
     cgames.color_game_2(esystem,"%s/slice_game_%s_n%04d"%(plotdir,sim,frame))
 
-if 1:
+if 0:
     #just e0 and e2
     cgames.color_game_3(esystem,"%s/slice_game_e0_e2_%s_n%04d"%(plotdir,sim,frame))
 
-if 1:
+if 0:
     cgames.color_game_4(esystem,"%s/slice_game_density_%s_n%04d"%(plotdir,sim,frame))
 
 if 1:
     cgames.color_game_5(esystem,"%s/slice_game_cutoff_%s_n%04d"%(plotdir,sim,frame))
+if 1:
+    cgames.color_game_6(esystem,"%s/slice_game_e2pos_%s_n%04d"%(plotdir,sim,frame))
 
 
 
