9a82faf40a83b73ad5cb22e1bf4fb48f1293bd66
David Collins <dccollins@fsu.edu>
2024-06-06 10:04:39 -0400

    now use dtools

--- python/GL.py
+++ python/GL.py
@@ -1,7 +1,15 @@
 #
 # Main python packages of my design.
 #
-from starter1 import *
+import os
+import sys
+if not os.path.exists('dtools/starter1.py'):
+    print("Need to get the submodule")
+    print("git submodule init")
+    print("git submodule update")
+    sys.exit(-1)
+from dtools.starter1 import *
+
 
 path_list=['tools','OtherPython']
 for directory in path_list:
