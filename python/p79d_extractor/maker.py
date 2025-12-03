8d14d307139d542153fa77e61230a6a066c60976
dcollins <dcollins4096@gmail.com>
2025-12-03 12:47:21 -0500

    update p79d maker/puller

--- python/p79d_extractor/maker.py
+++ python/p79d_extractor/maker.py
@@ -99,4 +99,60 @@ if 1:
     half=0
     suffix='23456'
 if 1:
-    puller.pull(sim_list, size, N_per_frame, target_res = target_res, suffix=suffix, rotate=rotate, los=los,half=half)
+    #sim_list = all_sims.lists['suite4']
+    sim_list=[]
+    for ns,sim in enumerate(all_sims.lists['suite4']):
+        if ns%4!=3:
+            sim_list.append(sim)
+    size=256
+    N_per_frame=5
+    rotate=False
+    target_res=64
+    los='xyz'
+    half=1
+    suffix='suite4_QU_'
+if 1:
+    #sim_list = all_sims.lists['suite4']
+    sim_list=[]
+    for ns,sim in enumerate(all_sims.lists['suite4']):
+        if ns%4!=3:
+            sim_list.append(sim)
+    size=256
+    N_per_frame=5
+    rotate=False
+    target_res=64
+    los='y'
+    half=0
+    suffix='fixed_mach_QU'
+if 1:
+    #sim_list = all_sims.lists['suite4']
+    sim_list=[]
+    for ns,sim in enumerate(all_sims.lists['suite4']):
+        if ns%4!=3:
+            sim_list.append(sim)
+    size=512
+    N_per_frame=5
+    rotate=False
+    target_res=64
+    los='y'
+    half=1
+    suffix='THQUEB'
+
+fields='THQUEB'
+if 1:
+    #sim_list = all_sims.lists['suite4']
+    sim_list=[]
+    for ns,sim in enumerate(all_sims.lists['suite4']):
+        if ns%4==3:
+            sim_list.append(sim)
+    size=512
+    N_per_frame=5
+    rotate=False
+    target_res=64
+    los='xyz'
+    half=0
+    suffix='T'
+    fields='T'
+print(sim_list)
+if 1:
+    puller.pull(sim_list, size, N_per_frame, target_res = target_res, suffix=suffix, rotate=rotate, los=los,half=half, fields=fields)
--- python/p79d_extractor/puller.py
+++ python/p79d_extractor/puller.py
@@ -17,31 +17,42 @@ def downsample_avg(x, M):
         raise ValueError("Input must be [N, N] or [B, C, N, N]")
 
 
-def pull(simlist, size, N_per_frame, target_res = None,suffix="", rotate=False, los='xyz', half=None):
+def pull(simlist, size, N_per_frame, target_res = None,suffix="", rotate=False, los='xyz', half=None, fields='THQUEB'):
     output = []
     quan = defaultdict(list)
+    if target_res:
+        adder = "_down_%d"%target_res
+    if rotate:
+        adder += "_rot"
+    whichhalf=''
+    if half is not None:
+        if half==0:
+            whichhalf = '_first'
+        if half==1:
+            whichhalf='_second'
+    oname = "p79d_subsets_S%d_N%d_%s_%s%s%s.h5"%(size,N_per_frame,los, adder, suffix,whichhalf)
+    print(oname)
     for sim in simlist:
         print(sim)
         this_sim = simulation.corral[sim]
         this_sim.read_avg_quan()
         sl = slice(None)
-        whichhalf=''
         if half is not None:
             if half==0:
                 sl = slice(0,len(this_sim.ann_frames)//2)
-                whichhalf = '_first'
             if half==1:
                 sl = slice(len(this_sim.ann_frames)//2, None)
-                whichhalf='_second'
 
 
         for frame in this_sim.ann_frames[sl]:
             for ilos,this_los in enumerate(los):
 
                 TEB={}
-                for field in 'TEB':
+                for field in fields:
                     if field == 'T':
                         field_name = 'density_'
+                    elif field == 'H':
+                        field_name = 'magnetic_field_strength_'
                     else:
                         field_name = field
                     frb_name = "%s/DD%04d.products/DD%04d_%s%s.fits"%(this_sim.product_location,frame,frame,field_name,this_los)
@@ -51,8 +62,8 @@ def pull(simlist, size, N_per_frame, target_res = None,suffix="", rotate=False, 
                 for n in range(N_per_frame):
                     corner = (np.random.random(2)*(Nside)).astype('int')
                     other = corner + np.array([size,size])
-                    TEB1 = np.zeros([3,size,size])
-                    for nf,field in enumerate('TEB'):
+                    TEB1 = np.zeros([len(fields),size,size])
+                    for nf,field in enumerate(fields):
                         TEB1[nf,:,:] = TEB[field][corner[0]:other[0], corner[1]:other[1]]
                     if rotate:
                         k=np.random.randint(0,4)
@@ -69,25 +80,26 @@ def pull(simlist, size, N_per_frame, target_res = None,suffix="", rotate=False, 
 
 
     Nsubs = len(output)
-    total = np.zeros([Nsubs, 3, size, size])
+    total = np.zeros([Nsubs, len(fields), size, size])
     inds = np.arange(Nsubs)
+    quan2 = {'Ms_mean':[],'Ma_mean':[],'Ms_act':[],'Ma_act':[],'los':[], 'frame':[]}
+    print('randomize')
     for n in inds:
         b = int((np.random.random()*len(output))//1)
         total[n,...] = output.pop(b)
+        for q in quan:
+            quan2[q].append( quan[q].pop(b))
         #print(len(output))
     total=np.array(total)
     total = torch.tensor(total,dtype=torch.float32)
     adder=""
     if target_res:
         total = downsample_avg(total, target_res)
-        adder = "_down_%d"%target_res
-    if rotate:
-        adder += "_rot"
-    oname = "p79d_subsets_S%d_N%d_xyz%s%s%s.h5"%(size,N_per_frame, adder, suffix,whichhalf)
     fptr = h5py.File(oname,'w')
+    print(oname)
     fptr['subsets']=total
     for q in quan:
-        fptr[q] = np.array(quan[q])
+        fptr[q] = np.array(quan2[q])
 
 
 
