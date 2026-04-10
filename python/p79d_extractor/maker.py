9c85198e94cdf91b162206ea2c7841000f29bc16
David Collins <dccollins@fsu.edu>
2026-04-10 10:25:55 -0400

    tweak field strength labels and extract more stuff

--- python/p79d_extractor/maker.py
+++ python/p79d_extractor/maker.py
@@ -215,19 +215,19 @@ if 1:
     suffix='suite7vs'
     fields='TVS'
 if 1:
-    sim_list=[]
-    for ns,sim in enumerate(all_sims.lists['suite1']):
-        if sim[0] not in ['4','5','6']:
-            continue
-        sim_list.append(sim)
+    sim_list=all_sims.lists['suite1']
+    #for ns,sim in enumerate(all_sims.lists['suite1']):
+    #    if sim[0] not in ['4','5','6']:
+    #        continue
+    #    sim_list.append(sim)
     size=128
     N_per_frame=5
     rotate=False
     target_res=128
     los='xyz'
     half=1
-    suffix='suite7_tvsquh_456'
-    fields='TVSQUH'
+    suffix='suite1_tvsquhp'
+    fields='TVSQUHP'
 print(sim_list)
 if 1:
     puller.pull(sim_list, size, N_per_frame, target_res = target_res, suffix=suffix, rotate=rotate, los=los,half=0, fields=fields)
