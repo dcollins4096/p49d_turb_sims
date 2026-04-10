0cd234805e9249a0a4c9800d1ad4b1c29251d66e
dcollins <dcollins4096@gmail.com>
2026-04-10 11:11:10 -0400

    endless tweaks to the batch scripts

--- python/data_scrub_2/batch_make_quan.py
+++ python/data_scrub_2/batch_make_quan.py
@@ -2,15 +2,14 @@ from  GL import *
 
 import simulation
 import simulation_info.all_sims as all_sims
-import simulation_info.suite_4 as s4
 
 import data_scrub_2.make_quan as caq
 import data_scrub_2.make_2d_spectra as m2d
 import data_scrub_2.make_3d_spectra as m3d
 import data_scrub_2.make_all_frb as maf
-import simulation_info.suite_5
-import simulation_info.suite_6_256
-import simulation_info.suite_7_128
+#import simulation_info.suite_5
+#import simulation_info.suite_6_256
+#import simulation_info.suite_7_128
 #this_list = simulation_info.suite_6_256.list_from_key[sys.argv[1]]
 this_list = [sys.argv[1]]
 
