4064c5737789f4517461a75ac3f6d0646f4bedd5
David Collins <dccollins@fsu.edu>
2026-08-21 09:39:35 -0400

    conflict in brano suite

--- python/simulation_info/brano_suite.py
+++ python/simulation_info/brano_suite.py
@@ -8,15 +8,23 @@ import re
 full_list=['d00_Ms1.0_Ma0.5_512', 'd01_Ms1.0_Ma1.5_512', 'd02_Ms1.0_Ma3.0_512', 'd03_Ms1.0_Ma0.0_512', 'd04_Ms2.0_Ma0.5_512', 'd05_Ms2.0_Ma1.5_512', 'd06_Ms2.0_Ma3.0_512', 'd07_Ms2.0_Ma0.0_512', 'd08_Ms3.0_Ma0.5_512', 'd09_Ms3.0_Ma1.5_512', 'd10_Ms3.0_Ma3.0_512', 'd11_Ms3.0_Ma0.0_512', 'd12_Ms4.0_Ma0.5_512', 'd13_Ms4.0_Ma1.5_512', 'd14_Ms4.0_Ma3.0_512', 'd15_Ms4.0_Ma0.0_512', 'd16_Ms5.0_Ma0.5_512', 'd17_Ms5.0_Ma1.5_512', 'd18_Ms5.0_Ma3.0_512', 'd19_Ms5.0_Ma0.0_512', 'd20_Ms6.0_Ma0.5_512', 'd21_Ms6.0_Ma1.5_512', 'd22_Ms6.0_Ma3.0_512', 'd23_Ms6.0_Ma0.0_512', 'd24_Ms7.0_Ma0.5_512', 'd25_Ms7.0_Ma1.5_512', 'd26_Ms7.0_Ma3.0_512', 'd27_Ms7.0_Ma0.0_512', 'd28_Ms8.0_Ma0.5_512', 'd29_Ms8.0_Ma1.5_512', 'd30_Ms8.0_Ma3.0_512', 'd31_Ms8.0_Ma0.0_512']
 
 rrr = re.compile(r'(...)_Ms(...)_Ma(...)_512')
-sim_dir_base = "/anvil/scratch/x-ux454321/Paper83/brano"
-product_dir_base = "/anvil/scratch/x-ux454321/Paper83/brano/products"
+if os.environ['machine'] == 'cloudbreak':
+    sim_dir_base = '/data/cb1/Projects/P79d_ML/simulations/brano_suite'
+    product_dir_base = '/data/cb1/Projects/P79d_ML/simulations/brano_suite/products'
+else:
+    sim_dir_base = dl.sim_dir_base
+    product_dir_base = dl.product_dir_base
+
 for sim in full_list:
     match = rrr.match(sim)
     name = match.group(0)
-    #short = match.group(1)
-    ms = float(match.group(2))
-    ma = float(match.group(3))
+    short = match.group(1)
+    ms = float( match.group(2))
+    ma = float( match.group(3))
+    framelist = list(range(102))
+    if sim.startswith('d15'):
+        framelist = list(range(10,45))
     simulation.sim(sim, data_location="%s/%s"%(sim_dir_base,name),
                    product_location="%s/%s"%(product_dir_base,name),
-                   ms=ms,ma=ma,framelist=list(range(10,102)))
+                   ms=ms,ma=ma,all_frames=list(range(10,102)),framelist=list(range(10,102)))
 
